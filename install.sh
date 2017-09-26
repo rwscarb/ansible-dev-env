67b4a8ef779c53137b06b558ff6e2f0ab56f0ac0
Ryan Scarbery <ryan.scarbery@gmail.com>
2017-09-26 14:26:21 -0700

    it compiles!

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
-#apt-get update
-#apt-get upgrade
-#apt-get install git python-pip
-#pip install ansible
-#USER=$(getent passwd 1000 | cut -f 1 -d ':')
-#mkdir -p /home/$USER/dev/github/
-#su - $USER
-#mkdir -p ~/dev/github/rwscarb
-#pushd ~/dev/github
-#mkdir rwscarb
-#pushd rwscarb
-#git clone https://github.com/rwscarb/dot-files.git
-#cd dot-files/
-#./install.sh 
-#popd
-#git clone https://github.com/rwscarb/ansible-dev-env.git
-#cd ansible-dev-env/
-#ansible-playbook -e user=ford playbook-bootstrap.yml -i inventory 
+set -e
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y git python-pip
+sudo pip install ansible
+sudo pip install --upgrade pyOpenSSL
+USER=$(getent passwd 1000 | cut -f 1 -d ':')
+mkdir -p ~/dev/github/rwscarb || true
+pushd ~/dev/github/rwscarb
+git clone https://github.com/rwscarb/dot-files.git || true
+cd dot-files/
+./install.sh 
+cd ..
+git clone https://github.com/rwscarb/ansible-dev-env.git || true
+cd ansible-dev-env/
+ansible-playbook -e user=ford playbook-bootstrap.yml -i inventory
+sudo reboot
